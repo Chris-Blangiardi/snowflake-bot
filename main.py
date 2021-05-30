3e26ca27cd13f73048cd20973be864ef1798350b
Chris-Blangiardi <75513086+Chris-Blangiardi@users.noreply.github.com>
2021-05-29 22:44:08 -0400

    10th embed test

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def googleSheets(ctx):
                   colour=0x0000FF, timestamp=datetime.datetime.utcnow())
     embed.add_field(name="Current Stats", value=None, inline=False)
     embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3"
-                            "%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png")"""
+                            "%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png")
 
     message = await ctx.send(embed=embed)
 
